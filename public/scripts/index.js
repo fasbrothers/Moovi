41925dda6782440bcd2f19afa530ac2c87013262
00010227 <4bis1.00010227@gmail.com>
2020-12-02 20:13:06 +0500

    added comments

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,12 @@
 
 // Auto Text
 
+// text appears in #h1 
+
 const autoText = "Welcome to Moovi website"
 let index = 0;
 
+// the function is for slicing autoText element, then each slice of word appears one by one in #h1
 function writeText() {
     document.getElementById("h1").innerText = autoText.slice(0, index);
     index++;
@@ -11,19 +14,26 @@ function writeText() {
     if (index > autoText.length) index = 0;
 }
 
+// interval of each word is 0.2s 
 setInterval(writeText, 200);
+
+
+
 // filtering images
-filterSelection("all")
+
+filterSelection("all") // execute the function and show all columns
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("column");
     if (c == "all") c = "";
+    // add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
         removeClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
     }
 }
 
+// show filtered elements
 function addClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
@@ -33,6 +43,7 @@ function addClass(element, name) {
     }
 }
 
+// hide elements that are not selected
 function removeClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
@@ -47,13 +58,15 @@ function removeClass(element, name) {
 
 // Accordion
 
-
+// grab question class by DOM
 const questions = document.querySelectorAll(".question");
+
 
 questions.forEach(question => {
     const btnn = question.querySelector(".question-btn");
     btnn.addEventListener("click", () => {
         questions.forEach(item => {
+            /* Toggle between hiding and showing the show-text class */
             if (item !== question) {
                 item.classList.remove("show-text")
             }
@@ -64,8 +77,10 @@ questions.forEach(question => {
 
 // Validation form
 
+// grab btnSubmit id by DOM
 const btnSubmit = document.getElementById('btnSubmit');
 
+//  this event is for poping up message, when the button is clicked 
 btnSubmit.addEventListener("click", e => {
     e.preventDefault();
 
